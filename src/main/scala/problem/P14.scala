4e5b5db40532cf330fa51a3519ed756b2b54bfa4
Jan Abt <jan.abt@smithsonianchannel.com>
2018-07-02 14:14:03 -0400

    formatting

--- src/main/scala/problem/P14.scala
+++ src/main/scala/problem/P14.scala
@@ -1,19 +1,18 @@
 package problem
+
 object P14 {
 
-  def dropNthItem[A](n:Int, aList: List[A]):List[A]  = {
-    def inner[A](c: Int, aList: List[A]): List[A] =
+  def dropNthItem[A](n: Int, aList: List[A]): List[A] = {
+
+    def inner[B<:A](c: Int, aList: List[B]): List[ B] =
       (c, aList) match {
-        case (_, Nil) => {
-          Nil
-        }
-        case (1, head :: tail) => {
-          inner(n, tail)
-        }
-        case (_, head :: tail) => {
-          head :: inner(c - 1, tail)
-        }
+        case (_, Nil) => Nil
+
+        case (1, head :: tail) => inner(n, tail)
+
+        case (_, head :: tail) => head :: inner(c - 1, tail)
       }
+
     inner(n, aList)
   }
 
