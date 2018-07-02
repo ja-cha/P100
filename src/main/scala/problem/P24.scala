1ffc12cd889c4e8fce0c20561978b302e9d5e71e
Jan Abt <jan.abt@smithsonianchannel.com>
2018-07-02 16:06:08 -0400

    formatting

--- src/main/scala/problem/P24.scala
+++ src/main/scala/problem/P24.scala
@@ -10,15 +10,17 @@ object P24 {
 
         val value = c.toString.toInt
 
-        val radix = (0 to i).foldLeft("") {
+        val radix = (0 to i).foldLeft("1") {
           (acc, i) =>
             if (i == 0)
-              acc + "1"
+              acc + ""
             else acc + "0"
         }.toInt
-        val n = radix * value
-      println( n)
-      println(  Math.sqrt(n).floor.toInt)
+        val n = (radix * value).toInt
+        val pow = Math.sqrt(n).floor.toInt
+
+        val w = Math.pow(2,pow).toInt
+      println( n + " = "+ w +"+"+ (n-w))
 
 
       }
---
+++ src/test/scala/problem/P24Speck.scala
@@ -0,0 +1,17 @@
+package problem
+
+import org.scalatest._
+
+
+class P24Speck
+  extends FlatSpec with Matchers {
+
+  "25" must "be 10111" in {
+    P24.
+      solution(25).
+      shouldBe {
+        "10111"
+      }
+  }
+
+  }
