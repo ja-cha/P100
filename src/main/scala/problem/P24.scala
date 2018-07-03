f9ab656305ab5c60b4f23d976a9fb207b1983850
jan_abt <jan_abt@Yahoo.com>
2018-07-03 07:33:07 -0400

    tests

--- src/main/scala/problem/P24.scala
+++ src/main/scala/problem/P24.scala
@@ -9,38 +9,34 @@ object P24 {
 
   def solution(number: Int):Int = {
 
-    val pairs = paired(number)
+
     //25 =
     //20 + 5
     //16 + 9
     //8 + 1
     // 16 + 8 + 1
 // 11001
-    println( pairs)
-    val l = pairs.map{
-       (pair)=>
+    println( paired(number))
+
+    val l = paired(number).foldLeft((new ListBuffer[Int](), 0))
+     {
+       case ((values, remainder), pair)=>
+
            val toThePowerOf = Math.sqrt(pair._1 * pair._2).floor.toInt
            val v =   Math.pow(2, toThePowerOf).toInt
            println( "2^" + toThePowerOf +"=" + v)
+         values += v
            val r =   pair._1 * pair._2 - v
-           println( "remainder:"+r)
+
+         println( "remainder:"+r)
 
          val p = paired(r)
            println( "-----")
 
-         p
+         (values, r)
     }
     println(l)
-//    pairs.map{ p =>
-//      val toThePowerOf = Math.sqrt(p._1 * p._2).floor.toInt
-//      val v =   Math.pow(2, toThePowerOf).toInt
-//      println( "2^" + toThePowerOf +"=" + v)
-//      val r =   p._1 * p._2 - v
-//      println( "remainder:"+r)
-//      println( "-----")
-//
-//      p
-//    }
+
 
        // val w = Math.pow(2,pow).toInt
 
