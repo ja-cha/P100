b876c6d57486fedad929437229a9a94bd26bda78
Jan Abt <jan.abt@smithsonianchannel.com>
2019-01-15 16:21:02 -0500

    test 25 clean up

--- src/main/scala/problem/P25.scala
+++ src/main/scala/problem/P25.scala
@@ -3,14 +3,14 @@ package problem
 
 object P25 {
 
-  def solution(l1:List[Int],l2:List[Int]): Map[Int, Int] = {
+  def solution(list1:List[Int],list2:List[Int]): Map[Int, Int] = {
 
   var min = Map.empty[Int, Int]
 
-    l1.foreach(n1 =>{
-      l2.foreach(n2 =>{
-          val current =  Math.abs(n1) + Math.abs(n2)
+    list1.foreach(n1 =>{
+      list2.foreach(n2 =>{
           val original = Math.abs(min.keys.sum) + Math.abs(min.values.sum)
+          val current =  Math.abs(n1) + Math.abs(n2)
           if(min.isEmpty | current < original){
             min = Map[Int, Int](n1 -> n2)
           }
--- src/test/scala/problem/P25Speck.scala
+++ src/test/scala/problem/P25Speck.scala
@@ -19,4 +19,17 @@ class P25Speck
       }
   }
 
+  "test 2" must "be Map(5->12)" in {
+
+    val l1 =   List(-12, 5, 84, 395, 12, -54, 156)
+
+    val l2 =   List(115, -126, -64, 48, 18, 12, 256)
+
+    P25.
+      solution(l1, l2).
+      shouldBe {
+        Map(5->12)
+      }
+  }
+
   }
