e976ad3e7dfe1a879228ae40c7136b8238149fca
Jan Abt <jan.abt@smithsonianchannel.com>
2019-01-16 09:20:43 -0500

    test 25 clean up

--- src/main/scala/problem/P25.scala
+++ src/main/scala/problem/P25.scala
@@ -1,22 +1,56 @@
 package problem
 
 
-object P25 {
+class P25(list1: List[Int], list2: List[Int]) {
 
-  def solution(list1:List[Int],list2:List[Int]): Map[Int, Int] = {
 
   var min = Map.empty[Int, Int]
 
-    list1.foreach(n1 =>{
-      list2.foreach(n2 =>{
-          val original = Math.abs(min.keys.sum) + Math.abs(min.values.sum)
-          val current =  Math.abs(n1) + Math.abs(n2)
-          if(min.isEmpty | current < original){
-            min = Map[Int, Int](n1 -> n2)
-          }
+
+  def solution(): Map[Int, Int] = {
+
+    list1.foreach(n1 => {
+      list2.foreach(n2 => {
+        val original = Math.abs(min.keys.sum) + Math.abs(min.values.sum)
+        val current = Math.abs(n1) + Math.abs(n2)
+        if (min.isEmpty | current < original) {
+          min = Map[Int, Int](n1 -> n2)
+        }
       })
     })
     min
   }
 
+  def recursiveSolution(): Map[Int, Int] = {
+
+    def recursive(l1: List[Int], l2: List[Int]): Map[Int, Int] =
+
+      l1 match {
+        case List() => Map.empty
+        case n1 :: _ => {
+          val n2 = l2.head
+          val original = Math.abs(min.keys.sum) + Math.abs(min.values.sum)
+          val current = Math.abs(n1) + Math.abs(n2)
+
+          if (min.isEmpty | current < original) {
+            min = Map[Int, Int](n1 -> n2)
+          }
+          if (l2.tail.isEmpty) {
+            recursive(l1.tail, list2)
+          }
+          else {
+            recursive(l1, l2.tail)
+          }
+        }
+          min
+      }
+
+    recursive(list1, list2)
+  }
+
+}
+
+object P25 {
+
+  def apply(list1: List[Int], list2: List[Int]): P25 = new P25(list1, list2)
 }
--- src/test/scala/problem/P25Speck.scala
+++ src/test/scala/problem/P25Speck.scala
@@ -6,27 +6,41 @@ import org.scalatest._
 class P25Speck
   extends FlatSpec with Matchers {
 
-  "test 1" must "be Map(5->4)" in {
+  "solution 1" must "be Map(5->4)" in {
 
     val l1 =   List(-12, 5, 84, 395, 12, 54, 156)
 
     val l2 =   List(15, 126, 64, 4, 18, 12, 256)
 
-    P25.
-      solution(l1, l2).
+    P25(l1, l2).
+      solution().
       shouldBe {
         Map(5->4)
       }
   }
 
-  "test 2" must "be Map(5->12)" in {
+  "solution 2" must "be Map(5->12)" in {
 
     val l1 =   List(-12, 5, 84, 395, 12, -54, 156)
 
     val l2 =   List(115, -126, -64, 48, 18, 12, 256)
 
-    P25.
-      solution(l1, l2).
+
+    P25(l1, l2).
+      solution().
+      shouldBe {
+        Map(5->12)
+      }
+  }
+
+ "recursiveSolution 2" must "be Map(5->12)" in {
+
+    val l1 =   List(-12, 5, 84, 395, 12, -54, 156)
+
+    val l2 =   List(115, -126, -64, 48, 18, 12, 256)
+
+   P25(l1, l2).
+      recursiveSolution().
       shouldBe {
         Map(5->12)
       }
