9c0ed8a39e04dabb71d95699ffcb21a0cbbad247
jan_abt <jan_abt@Yahoo.com>
2019-01-17 07:51:35 -0500

    recursive compare lists

--- src/main/scala/problem/P25.scala
+++ src/main/scala/problem/P25.scala
@@ -23,28 +23,25 @@ class P25(list1: List[Int], list2: List[Int]) {
 
   def recursiveSolution(): Map[Int, Int] = {
 
-    def recursive(l1: List[Int], l2: List[Int]): Map[Int, Int] =
+    def recursive(l1: List[Int], l2: List[Int]): Map[Int, Int] = {
 
-      l1 match {
-        case List() => Map.empty
-        case n1 :: _ => {
-          val n2 = l2.head
-          val original = Math.abs(min.keys.sum) + Math.abs(min.values.sum)
-          val current = Math.abs(n1) + Math.abs(n2)
-
-          if (min.isEmpty | current < original) {
-            min = Map[Int, Int](n1 -> n2)
-           // recursive(l1.tail, list2)
-          }
-          if (l2.tail.isEmpty) {
-            recursive(l1.tail, list2)
-          }
-          else {
-            recursive(l1, l2.tail)
-          }
-        }
-
+      if (l1.isEmpty) {
+        min
       }
+      else if (l2.tail.isEmpty) {
+        recursive(l1.tail, list2)
+      }
+      else {
+
+        val original = Math.abs(min.keys.sum) + Math.abs(min.values.sum)
+        val current = Math.abs(l1.head) + Math.abs(l2.head)
+
+        if (original == 0 | current < original) {
+          min = Map[Int, Int](l1.head -> l2.head)
+        }
+        recursive(l1, l2.tail)
+      }
+    }
 
     recursive(list1.sortWith(_ < _), list2.sortWith(_ < _))
   }
