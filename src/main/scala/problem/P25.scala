b1548507c31fc3bfaf4a6fae0846fbd38a92a2a8
jan_abt <jan_abt@Yahoo.com>
2019-01-17 21:43:40 -0500

    refactoring (wip)

--- src/main/scala/problem/P25.scala
+++ src/main/scala/problem/P25.scala
@@ -1,19 +1,18 @@
 package problem
 
 
-class P25(list1: List[Int], list2: List[Int]) {
-
-
-  var min = Map.empty[Int, Int]
+class P25(l1: List[Int], l2: List[Int]) {
 
 
   def solution(): Map[Int, Int] = {
 
-    list1.foreach(n1 => {
-      list2.foreach(n2 => {
-        val original = Math.abs(min.keys.sum - min.values.sum)
-        val current = Math.abs(n1 - n2)
-        if (min.isEmpty | current <= original) {
+    var min = Map.empty[Int, Int]
+
+    l1.foreach(n1 => {
+      l2.foreach(n2 => {
+        val originalDistance = Math.abs(min.keys.sum - min.values.sum)
+        val currentDistance = Math.abs(n1 - n2)
+        if (min.isEmpty | currentDistance <= originalDistance) {
           min = Map[Int, Int](n1 -> n2)
         }
       })
@@ -23,41 +22,45 @@ class P25(list1: List[Int], list2: List[Int]) {
 
   def recursiveSolution(): Map[Int, Int] = {
 
-    val list1Sorted = list1.sortWith(_ < _)
-    val list2Sorted = list2.sortWith(_ < _)
+    val list1 =  l1.sortWith(_ < _)
+    val list2 = l2.sortWith(_ < _)
 
-    def recursive(list1: List[Int], list2: List[Int]): Map[Int, Int] = {
+    def recursive(aList1: List[Int], aList2: List[Int], min: Map[Int, Int]): Map[Int, Int] = {
 
-      (list1, list2) match {
+      (aList1, aList2) match {
 
         case (l1, l2) if (l1.isEmpty) => {
           min
         }
         case (l1, l2) if (l2.isEmpty) => {
-          recursive(l1.tail, list2)
+          recursive(l1.tail, list2, min)
         }
         case (l1, l2) => {
 
-          val original = Math.abs(min.keys.sum - min.values.sum)
-          val current = Math.abs(l1.head - l2.head)
+          val currentDistance = Math.abs(l1.head - l2.head)
+          val originalDistance = Math.abs(min.keys.sum - min.values.sum)
 
-          if (min.isEmpty | current <= original) {
-            min = Map[Int, Int](l1.head -> l2.head)
+          if (min.isEmpty | currentDistance < originalDistance) {
+            recursive(l1, l2.tail, Map[Int, Int](l1.head -> l2.head))
           }
-          if (current == original) {
-            recursive(List.empty, List.empty)
+          else if (currentDistance == originalDistance) {
+            recursive(List.empty, List.empty, Map[Int, Int](l1.head -> l2.head))
           }
-          if (l1.head < l2.head) {
-            recursive(l1.tail, list2Sorted)
-          } else {
-            recursive(l1, l2.tail)
+          else {
+            if (l1.head <= l2.head) {
+              recursive(l1.tail, list2, min)
+            }
+            else {
+              recursive(l1, l2.tail, min)
+            }
           }
 
         }
       }
     }
 
-    recursive(list1Sorted, list2Sorted)
+    recursive(list1, list2, Map.empty)
+
   }
 
 }
--- src/test/scala/problem/P25Speck.scala
+++ src/test/scala/problem/P25Speck.scala
@@ -28,6 +28,27 @@ class P25Speck
     }
   }
 
+  "solution 2" must "be Map(5->4)" in {
+
+    val l1 = List(-12, 5, 84, 395, 123, 54, 156)
+
+    val l2 = List(15, 126, 64, 4, 18, 12, 256)
+
+    P25(l1, l2).solution().shouldBe {
+      Map(5 -> 4)
+    }
+  }
+ "recursiveSolution 2" must "be Map(5->4)" in {
+
+    val l1 = List(-12, 5, 84, 395, 123, 54, 156)
+
+    val l2 = List(15, 126, 64, 4, 18, 11, 256)
+
+    P25(l1, l2).recursiveSolution().shouldBe {
+      Map(5 -> 4)
+    }
+  }
+
   "solution 2" must "be Map(5->12)" in {
 
     val l1 = List(-12, 5, 84, 395, 489, -54, 156)
@@ -39,7 +60,7 @@ class P25Speck
     }
   }
 
-  "recursiveSolution 2" must "be Map(5->12)" in {
+  "recursiveSolution 3" must "be Map(5->12)" in {
 
     val l1 = List(-12, 5, 84, 395, 489, -54, 156)
 
