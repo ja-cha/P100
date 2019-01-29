5c8730ec0d5432cefb87d24fa8b765eb52971f2f
Jan Abt <jan.abt@smithsonianchannel.com>
2019-01-29 17:47:51 -0500

    use pattern matching

--- src/main/scala/problem/P25.scala
+++ src/main/scala/problem/P25.scala
@@ -26,30 +26,26 @@ class P25(l1: List[Int], l2: List[Int]) {
 
     def find(l1: List[Int], l2: List[Int], valuePair: (Int, Int)): (Int, Int) = {
 
-      if (l1.isEmpty) {
-        valuePair
-      }
-      else if (l2.isEmpty) {
-        find(l1.tail, sortedList2, valuePair)
-      }
-      else {
+      (l1, l2) match {
 
-        val currentDistance = Math.abs(l1.head.asInstanceOf[Long] - l2.head.asInstanceOf[Long])
-        val originalDistance = Math.abs(valuePair._1.asInstanceOf[Long] - valuePair._2.asInstanceOf[Long])
-
-        if (currentDistance == 0) {
-          find(List.empty, List.empty, (l1.head -> l2.head))
-        }
-        else if (currentDistance < originalDistance) {
-          find(l1, l2.tail, (l1.head -> l2.head))
-        }
-        else if (l1.head < l2.head) {
-          find(l1.tail, sortedList2, valuePair)
-        }
-        else {
-          find(l1, l2.tail, valuePair)
-        }
+        case (List(), _) => valuePair
+        case (_, List()) => find(l1.tail, sortedList2, valuePair)
+        case _ => val currentDistance = Math.abs(l1.head.asInstanceOf[Long] - l2.head.asInstanceOf[Long])
+          val originalDistance = Math.abs(valuePair._1.asInstanceOf[Long] - valuePair._2.asInstanceOf[Long])
+          if (currentDistance == 0) {
+            find(List.empty, List.empty, (l1.head -> l2.head))
+          }
+          else if (currentDistance < originalDistance) {
+            find(l1, l2.tail, (l1.head -> l2.head))
+          }
+          else if (l1.head < l2.head) {
+            find(l1.tail, sortedList2, valuePair)
+          }
+          else {
+            find(l1, l2.tail, valuePair)
+          }
       }
+
     }
 
     find(sortedList1, sortedList2, (Int.MinValue, Int.MaxValue))
