f37b2317d82e62801f26fbcce073c39bb0d4f11a
Jan Abt <jan.abt@smithsonianchannel.com>
2019-04-18 17:26:56 +0800

    variable renamed

--- src/main/scala/problem/P24.scala
+++ src/main/scala/problem/P24.scala
@@ -5,13 +5,13 @@ object P24 {
   def solution(number: Int): List[Int] = {
 
   val base2List =  asBase2List(number)
-  val base2ExpandedList  = asBase2ExpandedList(base2List, List.empty)
+  val binaryList  = asBinaryList(base2List, List.empty)
 
     println(s"  decimal (base  10) : $number")
     println(s"= binary  (base   2) : ${base2List.reverse.mkString(" + ")}")
-    println(s"= binary  (expanded) : ${base2ExpandedList.mkString(", ")}")
+    println(s"= binary  (expanded) : ${binaryList.mkString(", ")}")
     println("-------------------------------------------------------------")
-    base2ExpandedList
+    binaryList
   }
 
   /**
@@ -57,10 +57,10 @@ object P24 {
     * for example,
     * base 2 list (256, 8, 4, 1)
     * would be (1, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0)
-    * if presented in base 2 expanded format
+    * if presented in binary format
     *
     */
-  private def asBase2ExpandedList(base2List: List[Int], seedList:List[Int]):List[Int] = {
+  private def asBinaryList(base2List: List[Int], seedList:List[Int]):List[Int] = {
 
     val number = Math.pow(2, seedList.size).toInt
 
@@ -68,9 +68,9 @@ object P24 {
       case Nil => seedList
       case 0::Nil => 0::seedList
       case h::Nil if  h == number =>1::seedList
-      case h::Nil => asBase2ExpandedList (h::Nil, 0::seedList )
-      case h::tail if  h == number => asBase2ExpandedList(tail, 1::seedList)
-      case h::tail => asBase2ExpandedList(h::tail, 0::seedList)
+      case h::Nil => asBinaryList (h::Nil, 0::seedList )
+      case h::tail if  h == number => asBinaryList(tail, 1::seedList)
+      case h::tail => asBinaryList(h::tail, 0::seedList)
 
     }
   }
